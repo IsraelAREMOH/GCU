6bc629615ba4ddd7d0c1d3579215d861f840385b
Israel AREMOH <israelaremoh@gmail.com>
2026-02-18 07:03:48 +0000

    fix contact-form

--- src/components/Contact_FormSection.jsx
+++ src/components/Contact_FormSection.jsx
@@ -35,11 +35,14 @@ const Contact_FormSection = () => {
     setSuccess("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/contact`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        },
+      );
 
       const data = await response.json();
 
