70a2abef373e260a1ee14c308cddf1b5529c80c2
Israel AREMOH <israelaremoh@gmail.com>
2026-02-18 23:26:13 +0000

    Added a catcherr

--- src/components/Contact_FormSection.jsx
+++ src/components/Contact_FormSection.jsx
@@ -66,6 +66,8 @@ const Contact_FormSection = () => {
     } catch (err) {
       console.log(err);
       setErrors([{ msg: "Something went wrong. Try again later." }]);
+      console.log("REAL ERROR:", err);
+      setErrors([{ msg: err.message }]);
     }
   };
   return (
